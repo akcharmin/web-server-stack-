d6da3d81f5a8d37b61183c8d68ae3faf99f185ea
akcharmin <kareenatej@gmail.com>
2021-08-05 22:02:19 -0300

    git config --global user.name "akcharmin"

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "ap-south-1"
+    profile = "default"
+}
+
+/*
+provider "aws" {
+    region = "ap-south-1"
+    access_key = var.account_id
+    secret_key = var.account_pass
+}
+*/
